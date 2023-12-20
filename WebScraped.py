89741d3ded625b3ceb3452135702d0fad4c6fbe2
imvickykumar999 <imvickykumar999@gmail.com>
2023-12-20 16:38:49 +0530

    automated

--- WebScraped.py
+++ WebScraped.py
@@ -2,43 +2,48 @@
 import requests, os
 from bs4 import BeautifulSoup as bs
 
-link = 'https://www.upgrad.com/learn/data-analytics/sort-and-filter-in-excel-5406-32430-192355-592269-3026322/'
+link = 'https://www.upgrad.com/learn/'
 req = requests.get(link)
 soup = bs(req.content, 'html5lib')
 
-session = soup.findAll('div', attrs = {'class':'session'})
+def save_scrapped(link):
+    req = requests.get(link)
+    soup = bs(req.content, 'html5lib')
+
+    segment_heading = soup.findAll('h1', attrs = {'class':'segment_heading'})
+    title = segment_heading[0].text
+
+    text_component = soup.findAll('div', attrs = {'class':'text_component ckOutput'})
+    para = []
+
+    for i in text_component:
+        para.append(i.text)
+
+    folder = "".join(x for x in link.split('/')[4] if x.isalnum()) 
+    subfolder = "".join(x for x in link.split('/')[5] if x.isalnum()) 
+
+    try: os.mkdir('Scrapped')
+    except: pass
+
+    try: os.mkdir(f'Scrapped/{folder}')
+    except: pass
+
+    try: os.mkdir(f'Scrapped/{folder}/{subfolder}')
+    except: pass
+
+    text = f'''
+    <h1> {title} </h1>
+
+    <p> {[f'{i}<br><br>' for i in para]} </p>
+    '''
+
+    with open(f'Scrapped/{folder}/{subfolder}/{title}.html', 'w', encoding="utf-8") as f:
+        f.write(text)
+
+session = soup.findAll('div', attrs = {'class':'Carousel_item__UUZAx'})
 for i in session:
-    print(i.a['href'])
+    for j in i.findAll('a'):
 
-segment_heading = soup.findAll('h1', attrs = {'class':'segment_heading'})
-title = segment_heading[0].text
-print(title)
-
-text_component = soup.findAll('div', attrs = {'class':'text_component ckOutput'})
-para = []
-
-for i in text_component:
-    para.append(i.text)
-    for j in para:
-        print(j.split('.'))
-
-folder = link.split('/')[4]
-subfolder = link.split('/')[5]
-
-try: os.mkdir('Scrapped')
-except: pass
-
-try: os.mkdir(f'Scrapped/{folder}')
-except: pass
-
-try: os.mkdir(f'Scrapped/{folder}/{subfolder}')
-except: pass
-
-text = f'''
-<h1> {title} </h1>
-
-<p> {[f'{i}<br><br>' for i in para]} </p>
-'''
-
-with open(f'Scrapped/{folder}/{subfolder}/{title}.html', 'w', encoding="utf-8") as f:
-    f.write(text)
+        link = j['href']
+        try: save_scrapped(link)
+        except: pass
