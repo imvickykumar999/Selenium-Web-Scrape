594cfb2799854ef19f24a5c8591b913c6c88a1c4
imvickykumar999 <imvickykumar999@gmail.com>
2023-12-22 21:56:33 +0530

    one page scrapped

--- ScrapeThisSite/Forms, Searching & Pagination.py
+++ ScrapeThisSite/Forms, Searching & Pagination.py
@@ -1,6 +1,7 @@
 
 import requests, os
 from bs4 import BeautifulSoup as bs
+import pandas as pd
 
 try: os.mkdir('Scrapped')
 except: pass
@@ -9,10 +10,29 @@ link = 'https://www.scrapethissite.com/pages/forms/?page_num=1&per_page=25'
 req = requests.get(link)
 soup = bs(req.content, 'html5lib')
 
+data = {}
 table = soup.findAll('table', attrs = {'class':'table'})[0]
-for i in table:
-    tr = table.findAll('tr')
+tr = table.findAll('tr')
 
-    for j in tr:
-        print(j.text.strip()) # append this in list
-    # and update in new dict above list
+for i, j in enumerate(tr):
+    lst = []
+    if i == 0:
+        th = j.findAll('th')
+        for m in th:
+            lst.append(m.text.strip())
+    else:
+        td = j.findAll('td')
+        for n in td:
+            lst.append(n.text.strip())
+    data.update({i : lst})
+
+df = pd.DataFrame.from_dict(
+    data, 
+    orient='index'
+)
+
+df.to_csv(
+    'Scrapped/Forms, Searching & Pagination.csv', 
+    index = False, 
+    encoding='utf-8'
+)
